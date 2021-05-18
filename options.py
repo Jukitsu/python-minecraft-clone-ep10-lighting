0b5a62a8a210e8a65c5a238f0b26e272f216c407
Jukitsu <84381972+Jukitsu@users.noreply.github.com>
2021-05-18 15:43:07 +0200

    Added Lighting type setting and descriptions

--- options.py
+++ options.py
@@ -1,12 +1,15 @@
-LIGHTING = True
+LIGHTING = True # Enable Lighting
 
 DYNAMIC_LIGHTING = LIGHTING  # Totally useless variable, but is used sometimes instead of LIGHTING bc I forgot to
 # change it
 
-BRIGHTNESS = 1
+LIGHTING_TYPE = "EXPONENTIAL"  # Exponential -> Alpha lighting, darker
+                               # Linear -> Indev lighting, brighter
+
+BRIGHTNESS = 1  # Brightness setting, 0 is Moody, 1 is Bright
 
 
-MIPMAP = True
-MIPMAP_TYPE = "LINEAR"
-SMOOTHING = True
-BLUR = False
+MIPMAP = True  # Enable Mipmaps
+MIPMAP_TYPE = "LINEAR"  # Nearest -> Rough Smoothing / Linear -> Linear Smoothing
+SMOOTHING = True  # Enable Minification Filter
+BLUR = False  # Linearizes magnified textures (cool for HD textures I guess ?)
